5710b84c08e56fbae8804f5eac2f580c1df75bd3
Tina Smith <smithtina1203@aol.com>
2025-07-06 18:48:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import WordForm from './components/WordForm/WordForm';
+// import CardContainer from './components/CardsContainer/CardContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>To-Do-List</h1>
+        </header>
+        <main>
+         
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
