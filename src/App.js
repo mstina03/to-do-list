f40461f163a5a3423b1014edd413b89360c485e8
Tina Smith <smithtina1203@aol.com>
2025-07-08 11:28:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,24 +8,18 @@ import ContactContainer from './components/ContactContainer/ContactContainer';
 import './components/TaskContainer/TaskContainer.css';
 import './components/ContactContainer/ContactContainer.css';
 
-
 function App() {
-
   const [tasks, setTasks] = useState([
     { front: 'Take package to post office', back: '' },
     { front: 'Pay electric bill', back: 'Due the 5th of the month' },
     { front: 'Pay water bill', back: 'Due the 15th of the month' },
     { front: 'Pay cable bill', back: 'Due the 20th of the month' },
     { front: 'Pay student loan', back: 'Due the 25th of the month' },
-
-
   ]);
 
   const [contacts, setContacts] = useState([
     { front: 'Jane Doe', back: '555-1234' },
     { front: 'John Smith', back: 'john@example.com' },
-
-
   ]);
 
   console.log(contacts);
@@ -34,66 +28,50 @@ function App() {
 
   const addTask = (front, back) => {
     const newTasks = [...tasks, { front, back }];
-
     setTasks(newTasks);
-  }
+  };
 
   const deleteTask = (front) => {
     const newTasks = tasks.filter(task => task.front !== front);
     setTasks(newTasks);
-
-  }
+  };
 
   const addContact = (front, back) => {
     const newContacts = [...contacts, { front, back }];
-
     setContacts(newContacts);
-  }
-
-
+  };
 
   const deleteContact = (id) => {
     const updatedContacts = contacts.filter((_, index) => index !== id);
     setContacts(updatedContacts);
   };
 
- 
-  
-
   return (
     <div className="App">
       <NavBar />
-      <header className="App-header">
+      <header className="App-header" />
 
-      </header>
-      <main >
-
+      <main>
         <section id="addnew">
           <TaskForm addTask={addTask} />
         </section>
-
 
         <section id="tasks">
           <h1>Task List</h1>
           <TaskContainer tasks={tasks} deleteTask={deleteTask} />
         </section>
 
-        <section id='addcontact'>
-
+        <section id="addcontact">
           <ContactForm addContact={addContact} />
-
         </section>
 
-        <section id='contacts'>
+        <section id="contacts">
           <h1>Contact List</h1>
           <ContactContainer contacts={contacts} deleteContact={deleteContact} />
         </section>
-
-
       </main>
     </div>
   );
 }
-
 
 export default App;
